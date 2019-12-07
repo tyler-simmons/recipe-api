6d1965231b07a0d68691c36be3f36bbecf49a049
Tyler Simmons <tyler.simmons1996@gmail.com>
2019-12-07 02:54:03 -0500

    removed old files

---
+++ test.js
@@ -0,0 +1,13 @@
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: '192.168.1.188',
+    user: 'apiuser',
+    password: '2147',
+    database: 'recipe_db'
+});
+
+
+
+
+
